bdb6d067ccf722917cb27ae421fb7cecbab76e96
Timm Knape <timm.knape@adamhall.com>
2019-08-20 09:35:36 +0200

    remove duplicates

--- src/hx-srcs.sh
+++ src/hx-srcs.sh
@@ -5,11 +5,11 @@ USED=""
 
 while [[ "x$FILES" != "x" ]]; do
 	i=`expr match "${FILES}" '[^ ]* '`
-	f=${FILES:0:$i}
-	FILES=${FILES#$f}
-	j=`expr match "${USED}" '(.* )*$f '`
-	if [[ "$j" = "0" ]]; then
-		echo $f
+	f=${FILES:0:$((i - 1))}
+	FILES=${FILES#$f }
+	j=`expr match "${USED}" "\(\(.* \)*$f \)"`
+	if [[ "$j" = "" ]]; then
+		echo "$f"
 		USED="$f $USED"
 		NEW=`grep '@inc(' $f | cut '-d(' -f2 | cut '-d)' -f1`
 		TOADD=''
